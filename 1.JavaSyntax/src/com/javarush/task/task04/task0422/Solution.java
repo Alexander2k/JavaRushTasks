2bf24cf363bf9502acf1116878819da90fc2afe1
alexander.goncharov <morfi2004@list.ru>
2019-10-13 13:56:11 +0300

    Done Task

---
+++ 1.JavaSyntax/src/com/javarush/task/task04/task0422/Solution.java
@@ -0,0 +1,15 @@
+package com.javarush.task.task04.task0422;
+
+/* 
+18+
+*/
+
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        //напишите тут ваш код
+
+    }
+}
